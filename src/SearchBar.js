65a482427319bb75dc132ac7663217c0d9461a0f
samir692 <samir.huseynzade.966@gmail.com>
2019-11-12 21:12:05 +0100

    Search functionality is added

---
+++ src/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SearchBar = ({  searchField, searchChange }) => {
+    return (
+        <div className="pa2">
+            <input className="pa3 b--green bg-lightest-blue" type="search" 
+                placeholder="search robots" onChange={searchChange}/>
+        </div>
+    );
+}
+
+export default SearchBar;
