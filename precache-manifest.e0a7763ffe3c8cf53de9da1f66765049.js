177e270a07774a3551c483cd99f1b2a4aede2867
samir692 <samir.huseynzade.966@gmail.com>
2019-11-12 22:59:31 +0100

    Updates

---
+++ precache-manifest.e0a7763ffe3c8cf53de9da1f66765049.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38f2578dc2566dbe8e6aa155ee4cf70",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "203451e9aa66d027310a",
+    "url": "/robo-friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "dd78c23490d8a774e8a6",
+    "url": "/robo-friends/static/css/main.89a4a4a7.chunk.css"
+  },
+  {
+    "revision": "203451e9aa66d027310a",
+    "url": "/robo-friends/static/js/2.e8ac3de4.chunk.js"
+  },
+  {
+    "revision": "dd78c23490d8a774e8a6",
+    "url": "/robo-friends/static/js/main.84303fb9.chunk.js"
+  },
+  {
+    "revision": "45d0c7ad396a754b3593",
+    "url": "/robo-friends/static/js/runtime-main.098cd101.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
