253c2158bceac72e1586ba748aa62f796e0aecb2
samir692 <samir.huseynzade.966@gmail.com>
2019-11-12 22:01:01 +0100

    dummy data is replaced with api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component }  from "react";
 import  CardList  from "./CardList";
 import SearchBar from './SearchBar'
-import {robots} from './robots';
 import './App.css';
 
 class App extends Component {
@@ -10,9 +9,15 @@ class App extends Component {
     constructor(){
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ""
         }
+    }
+
+    componentDidMount(){
+        fetch("http://jsonplaceholder.typicode.com/users")
+        .then(response =>  response.json())
+        .then(users => this.setState({robots: users}));
     }
 
     onSearchChange = (event) =>  {
@@ -24,13 +29,19 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         });
-        return (
-            <div className="tc">
-                <h1 className="f1">Robo Friends App</h1>
-                <SearchBar searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+
+        if(this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        }
+        else{
+            return (
+                <div className="tc">
+                    <h1 className="f1">Robo Friends App</h1>
+                    <SearchBar searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
     }
 }
 
