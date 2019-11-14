71aaeb87f167c2736eb7c00cb22983e4576d4235
samir692 <samir.huseynzade.966@gmail.com>
2019-11-14 21:28:52 +0100

    redux is added, store is created, redux is connected to react app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,29 @@
 import React, { Component }  from "react";
+import { connect } from 'react-redux';
 import  CardList  from "../components/CardList";
 import SearchBar from '../components/SearchBar'
 import './App.css';
 import Scroll from "../components/Scroll";
 
+import { setSearchField } from "../actions";
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchRobots.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
 class App extends Component {
     constructor(){
         super();
         this.state = {
-            robots: [],
-            searchField: ""
+            robots: []
         }
     }
 
@@ -19,12 +33,9 @@ class App extends Component {
         .then(users => this.setState({robots: users}));
     }
 
-    onSearchChange = (event) =>  {
-        this.setState({searchField: event.target.value});
-    }
-
     render(){
-        const { robots, searchField } = this.state;
+        const { robots } = this.state;
+        const { searchField } = this.state;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
@@ -44,4 +55,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
